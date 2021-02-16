bc49fe92416b1ba11d493fd8e70403fb22efaebd
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2021-02-16 16:01:11 +0530

    spelling

--- genjs.js
+++ genjs.js
@@ -1,5 +1,5 @@
 var eqns=['Finklestein Reaction',' Swarts Reaction','Hunsdiecker Reaction','Birnbaum Simonini Reaction','Williamson\'s Synthesis','Wurtz Reaction','Frankland\'s Reaction','Sandmeyer Reaction','Gattermann\'s Reaction','Balz-Schiemann Reaction','Fittig Reaction','Wurtz Fittig Reaction','Friedel Crafts Alkylation','Friedel Crafts Acylation','NBS','Reimer Tiemann Reaction','Kolbe\'s Reaction','Rosenmund\'s Reaction',
-'Gattermann Koch Synthesis','Friedel Crafts Formylation','Wolff Kishner Reduction','Haloform Reaction','Cannizaro\'s Reaction','Hell Volhard Zelinsky Reaction','Gabriel Phthalimide Synthesis','Hoffman Bromamide Degradation','Carbylamine Reaction','Diazotisation','Bromine Water Test for Glucose'];
+'Gattermann Koch Synthesis','Friedel Crafts Formylation','Wolff Kishner Reduction','Haloform Reaction','Cannizaro\'s Reaction','Hell Volhard Zelinsky Reaction','Gabriel Phthalimide Synthesis','Hoffmann Bromamide Degradation','Carbylamine Reaction','Diazotisation','Bromine Water Test for Glucose'];
 function NewEqn()
 {
 var randomeqn= Math.floor(Math.random()* (eqns.length));
